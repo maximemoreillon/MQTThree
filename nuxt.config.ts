43b4d8f2229d502f0c4ce08f76498b459beffe1d
Maxime Moreillon <m.moreillon@gmail.com>
2023-04-20 15:13:47 +0900

    env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,11 @@
 import vuetify from "vite-plugin-vuetify"
 
-const {
-  NUXT_MQTT_HOST,
-  NUXT_MQTT_PORT,
-  NUXT_MQTT_USERNAME,
-  NUXT_MQTT_PASSWORD,
-} = process.env
-
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      mqttHost: NUXT_MQTT_HOST,
-      mqttPort: NUXT_MQTT_PORT,
+      mqttHost: "",
+      mqttPort: "",
     },
-    mqttUsername: NUXT_MQTT_USERNAME,
-    mqttPassword: NUXT_MQTT_PASSWORD,
   },
   build: {
     transpile: ["vuetify"],
