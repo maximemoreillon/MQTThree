fdbc714c980106355f06b7d2ced73d48c9d85c5f
Maxime Moreillon <m.moreillon@gmail.com>
2024-11-03 12:47:28 +0900

    pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -5,30 +5,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		SvelteKitPWA({
-			// Not sure if necessary
-			// https://vite-pwa-org.netlify.app/guide/pwa-minimal-requirements.html#web-app-manifest
-			manifest: {
-				name: 'MQTThree',
-				short_name: 'MQTThree',
-				description: 'MQTT + Threejs',
-				theme_color: '#000',
-				icons: [
-					{
-						src: 'pwa-192x192.png',
-						sizes: '192x192',
-						type: 'image/png'
-					},
-					{
-						src: 'pwa-512x512.png',
-						sizes: '512x512',
-						type: 'image/png'
-					}
-				]
-			},
-			// This is not an error: https://vite-pwa-org.netlify.app/frameworks/sveltekit.html#sveltekit-pwa-plugin-options
-			adapterFallback: 'index.html'
-		})
+		SvelteKitPWA(
+			{ registerType: 'autoUpdate' }
+		)
 	],
 	ssr: {
 		noExternal: ['three']
